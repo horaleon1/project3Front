7e23df21b77cd18995347283a5864ac9aef85807
horaleon1 <horaleon1@gmail.com>
2019-09-18 13:24:02 -0500

    "build"

---
+++ build/precache-manifest.25611af3b0f0e677758cdffab3c1a8fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ba6c56fbf158ae565b1490fe48a54a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bd29b3a5eb66ae4dc354",
+    "url": "/static/css/main.a9fb5823.chunk.css"
+  },
+  {
+    "revision": "8ec96bb3d9cd1b8ee76f",
+    "url": "/static/js/2.deaad62b.chunk.js"
+  },
+  {
+    "revision": "bd29b3a5eb66ae4dc354",
+    "url": "/static/js/main.6da3c733.chunk.js"
+  },
+  {
+    "revision": "4950d10d9d48d6d2a9e8",
+    "url": "/static/js/runtime~main.58f5033b.js"
+  }
+]);
