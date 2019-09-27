9f15994f00337f576f815fae9fe16c6d84a2d8ac
horaleon1 <horaleon1@gmail.com>
2019-09-26 21:57:22 -0500

    link to footer and social networks

--- src/components/layouts/Footer.js
+++ src/components/layouts/Footer.js
@@ -1,27 +1,55 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+
 
 const footer = () => {
   return (
     <footer className="footerContainer">
       <div>
-       <ul className="listFooter">
-         <li> Home </li>
-         <li> About </li>
-         <li> Sign in </li>
-         <li> Sign up </li>                  
-       </ul>
-      </div>
-      <div>
-        <ul className="listSocialFooter">
-         <li>Facebook</li>
-         <li>Twitter</li>
+        <ul className="listFooter">
+          <li>
+            <Link to="/" style={{ textDecoration: "none", color: "white" }}>
+              Inicio
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/about"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              ¿Qué es Blockchain?
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/login"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Iniciar Sesión
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/register"
+              style={{ textDecoration: "none", color: "white" }}
+            >
+              Regístrase
+            </Link>
+          </li>
         </ul>
       </div>
       <div>
-       Copyright &copy; Blockchain Technologies {new Date().getFullYear()} all rights reserved
+        <ul className="listSocialFooter">
+          <li> <a href="www.facebook.com" target="_blank">Facebook</a></li>
+          <li> <a href="www.twitter.com" target="_blank">Twitter</a></li>
+        </ul>
+      </div>
+      <div>
+        Copyright &copy; Blockchain Technologies {new Date().getFullYear()} all
+        rights reserved
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default footer
+export default footer;
---
+++ src/context/alert/AlertState.js
@@ -0,0 +1,108 @@
+import React, { useReducer } from 'react';
+import axios from 'axios';
+import AlertContext from './AlertContext';
+import alertReducer from './alertReducer';
+
+
+// import setAuthToken from '../../utils/setAuthToken';
+
+import {
+  SET_ALERT,REMOVE_ALERT
+} from '../types';
+
+const AuthState = props => {
+  const initialState = [];
+
+  const [state, dispatch] = useReducer( alertReducer, initialState);
+
+  const setAlert = (msg, type) => {
+
+  }
+
+  // set Alert
+  // const loadUser = async () => {
+  //   if (localStorage.token) {
+  //     setAuthToken(localStorage.token);
+  //   }
+
+  //   try {
+  //     const res = await axios.get('/api/auth');
+
+  //     dispatch({
+  //       type: USER_LOADED,
+  //       payload: res.data
+  //     });
+  //   } catch (err) {
+  //     dispatch({ type: AUTH_ERROR });
+  //   }
+  // };
+
+  // Register User
+  // const register = async formData => {
+  //   const config = {
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   };
+
+  //   try {
+  //     const res = await axios.post('/api/users', formData, config);
+
+  //     dispatch({
+  //       type: REGISTER_SUCCESS,
+  //       payload: res.data
+  //     });
+
+  //     loadUser();
+  //   } catch (err) {
+  //     dispatch({
+  //       type: REGISTER_FAIL,
+  //       payload: err.response.data.msg
+  //     });
+  //   }
+  // };
+
+  // Login User
+  // const login = async formData => {
+  //   const config = {
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   };
+
+  //   try {
+  //     const res = await axios.post('/api/auth', formData, config);
+
+  //     dispatch({
+  //       type: LOGIN_SUCCESS,
+  //       payload: res.data
+  //     });
+
+  //     loadUser();
+  //   } catch (err) {
+  //     dispatch({
+  //       type: LOGIN_FAIL,
+  //       payload: err.response.data.msg
+  //     });
+  //   }
+  // };
+
+  // Logout
+  // const logout = () => dispatch({ type: LOGOUT });
+
+  // Clear Errors
+  // const clearErrors = () => dispatch({ type: CLEAR_ERRORS });
+
+  return (
+    <AlertContext.Provider
+      value={{
+        alerts: state,
+        setAlert
+      }}
+    >
+      {props.children}
+    </AlertContext.Provider>
+  );
+};
+
+export default AuthState;
