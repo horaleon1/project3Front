2c47aa79d76f7268b3586d1242b6d3a5bb3a67ac
horaleon1 <horaleon1@gmail.com>
2019-09-29 15:15:34 -0500

    changes nothing o user

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -1,33 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const User = () => {
-  return (
-    <div>
-      <div className="sidebarUser">
-        <div className="barUser">
+export default class extends Component {
+  // constructor(props) {
+  //   super(props);
 
+  //   this.state = {
+  //     cryptos: []
+  //   };
+  // }
+  // componentDidMount(){
+  //   axios.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR')
+  //   .then(res => {
+  //     const cryptos = res.data;
+  //     console.log(cryptos);
+  //     this.setState({cryptos: cryptos});
+  //   })
+  // }
+
+  render() {
+    return (
+      <div>
+        <div className="sidebarUser">
+          <div className="barUser">BTC</div>
+          <ul>
+            <li className="userMenu">
+              <ul className="userMenuList">
+                <li>
+                  <a>Inicio</a>
+                </li>
+                <li>
+                  <a>Enviar</a>
+                </li>
+                <li>
+                  <a>Recibir</a>
+                </li>
+                <li>
+                  <a>Ayuda</a>
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
-        <ul>
-          <li className="userMenu">
-            <ul className="userMenuList">
-              <li>
-                <a>Inicio</a>
-              </li>
-              <li>
-                <a>Enviar</a>
-              </li>
-              <li>
-                <a>Recibir</a>
-              </li>
-              <li>
-                <a>Ayuda</a>
-              </li>
-            </ul>
-          </li>
-        </ul>
       </div>
-    </div>
-  );
-};
-
-export default User;
+    );
+  }
+}
