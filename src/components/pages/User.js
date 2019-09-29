6f8746e77131c69441da8c08af333c5bb3c2eae1
horaleon1 <horaleon1@gmail.com>
2019-09-29 12:29:52 -0500

    "User styles"

--- src/components/pages/User.js
+++ src/components/pages/User.js
@@ -4,22 +4,28 @@ const User = () => {
   return (
     <div>
       <div className="sidebarUser">
+        <div className="barUser">
+
+        </div>
         <ul>
-          <li>
-            <a>Inicio</a>
-          </li>
-          <li>
-            <a>Enviar</a>
-          </li>
-          <li>
-            <a>Recibir</a>
-          </li>
-          <li>
-            <a>Ajustes</a>
+          <li className="userMenu">
+            <ul className="userMenuList">
+              <li>
+                <a>Inicio</a>
+              </li>
+              <li>
+                <a>Enviar</a>
+              </li>
+              <li>
+                <a>Recibir</a>
+              </li>
+              <li>
+                <a>Ayuda</a>
+              </li>
+            </ul>
           </li>
         </ul>
       </div>
-      
     </div>
   );
 };
