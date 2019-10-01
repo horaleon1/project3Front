6f0aba367330a576b83d926e6d06924ed6b8aa71
horaleon1 <horaleon1@gmail.com>
2019-09-30 20:19:40 -0500

    working on Portfolio

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, {Component} from "react";
+// import styled from "styled-components";
 
-export default function Portfolio() {
-  return (
+const cc = require("cryptocompare");
+
+export default class extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      coinList:[]
+    }
+  }
+
+  componentDidMount = () => {
+    this.fetchCoins();
+  };
+
+  fetchCoins = async () => {
+    let coinList = (await cc.coinList()).Data;
+    console.log(coinList);
+    this.setState = ({ coinList });
+  };
+  render (){
+    return(
     <div className="portfolioContainer">
-      
+      <div className="barPortfolio">
+        <div>NAME</div>
+        <div />
+        <div>Dashboard</div>
+        <div>Settings</div>
+      </div>
+      <div className="list">
+        <ul>
+          <li>
+          Coins:
+          <span>
+        {this.state.coinList.LTC}           
+          </span>
+          </li>
+        </ul>
+      </div>
     </div>
-  )
+    )
+  }
 }
